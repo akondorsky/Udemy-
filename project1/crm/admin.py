3206c35760d6439c98383284889e17f42cfe8878
akondorsky@gmail.com <akondorsky>
2022-09-08 12:28:55 +0200

    CRM App finished

--- project1/crm/admin.py
+++ project1/crm/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, StatusCrm, ComentCrm
 
 
 # Register your models here.
 admin.site.register(Order)
+admin.site.register(StatusCrm)
+admin.site.register(ComentCrm)
+
+
 
